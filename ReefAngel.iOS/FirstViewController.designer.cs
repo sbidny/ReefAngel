58c9bd3b5bbcc7e416080a27a809b86cce91d075
Steven Bidny <sbidny@gelbergroup.com>
2013-12-10 18:34:44 -0600

    Testing the XIB Editor

--- ReefAngel.iOS/FirstViewController.designer.cs
+++ ReefAngel.iOS/FirstViewController.designer.cs
@@ -1,17 +1,26 @@
+// WARNING
 //
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
 
 namespace ReefAngel
 {
-    [Register("FirstViewController")]
-    partial class FirstViewController
-    {
-        void ReleaseDesignerOutlets()
-        {
-        }
-    }
+	[Register ("FirstViewController")]
+	partial class FirstViewController
+	{
+		[Outlet]
+		MonoTouch.UIKit.UILabel lblTest { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (lblTest != null) {
+				lblTest.Dispose ();
+				lblTest = null;
+			}
+		}
+	}
 }
